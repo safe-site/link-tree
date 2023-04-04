48faff2d5177e9ad120487e4d9ca8c28bf76afa5
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-04-04 13:00:00 +0530

    Update linktree.js

--- linktree.js
+++ linktree.js
@@ -1,8 +1,26 @@
 
-  window.onload = function() {
-  document.getElementById('myImage').addEventListener('click', function() {
-    window.location.href = this.parentElement.href;
-    this.parentElement.download = '';
-    this.parentElement.click();
+  window.addEventListener("load", function() {
+  var img = document.querySelector("header img");
+  var link = document.createElement("a");
+  link.href = img.src;
+  link.download = "header-img";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+// function to add a click listener to the WhatsApp floating button
+function addWhatsAppListener() {
+  const waBtn = document.querySelector(".floating-button");
+  waBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    const message = "Hi, Shashi";
+    const number = "919508914855";
+    const url = "https://wa.me/" + number + "?text=" + encodeURIComponent(message);
+    window.open(url, "_blank");
   });
-};
+}
+
+// call the function to add the WhatsApp click listener
+addWhatsAppListener();
+
