af7fb179af380c245eaf62b72a3d450dbd53f8b2
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-04-10 12:18:44 +0530

    Update linktree.js

---
+++ linktree.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    $(document).ready(function () {
+        $("#myLink").click(function () {
+            // Handler function for click event
+            alert("Link clicked!");
+        });
+    });
+</script>
