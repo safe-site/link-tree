1eeb5daf417b8c478dfc7b82c988a075a152a084
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-04-17 16:47:56 +0530

    Update linktree.js

--- linktree.js
+++ linktree.js
@@ -6,3 +6,11 @@
         });
     });
 </script>
+window.addEventListener("scroll", function() {
+  var header = document.getElementById("myHeader");
+  if (window.pageYOffset > 200) {
+    header.classList.add("show");
+  } else {
+    header.classList.remove("show");
+  }
+});
