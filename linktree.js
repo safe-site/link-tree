70961eba3df15ff62f488795bb7e771566cd56a8
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-04-17 20:31:24 +0530

    Update linktree.js

--- linktree.js
+++ linktree.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function() {
   var header = document.getElementById("myHeader");
-  if (window.pageYOffset > 200) {
+  if (window.pageYOffset > 100) {
     header.classList.add("show");
   } else {
     header.classList.remove("show");
