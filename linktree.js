16137c899e49db56925e91d850c8d415c5035b0c
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-04-17 16:52:41 +0530

    Update linktree.js

--- linktree.js
+++ linktree.js
@@ -1,16 +1,19 @@
-<script type="text/javascript">
-    $(document).ready(function () {
-        $("#myLink").click(function () {
-            // Handler function for click event
-            alert("Link clicked!");
-        });
-    });
+<script type="text/javascript" src="jquery.js"></script> <!-- Include the jQuery library -->
+
+<script type="text/javascript"> 
+   $(document).ready(function () { 
+       $("#myLink").click(function () { 
+           // Handler function for click event 
+           alert("Link clicked!"); 
+       }); 
+   }); 
+
+   window.addEventListener("scroll", function() { 
+      var header = document.getElementById("myHeader"); 
+      if (window.pageYOffset > 200) { 
+        header.classList.add("show"); 
+      } else { 
+        header.classList.remove("show"); 
+      } 
+   });
 </script>
-window.addEventListener("scroll", function() {
-  var header = document.getElementById("myHeader");
-  if (window.pageYOffset > 200) {
-    header.classList.add("show");
-  } else {
-    header.classList.remove("show");
-  }
-});
