329abdadd8c833d561d1c85714fef2f16f859f6a
Shashi Kumar <121589680+9508914855@users.noreply.github.com>
2023-05-21 10:50:34 +0530

    Update linktree.js

--- linktree.js
+++ linktree.js
@@ -7,3 +7,35 @@ window.addEventListener("scroll", function() {
   }
 });
 
+var popupButton = document.getElementById("popupButton");
+var overlay = document.getElementById("overlay");
+var popupContainer = document.getElementById("popupContainer");
+
+popupButton.addEventListener("click", function() {
+  overlay.style.display = "block";
+  popupContainer.style.display = "block";
+});
+
+overlay.addEventListener("click", function() {
+  overlay.style.display = "none";
+  popupContainer.style.display = "none";
+});
+
+function openPopup() {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("popupContainer").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("popupContainer").style.display = "none";
+  document.getElementById("iframePopup").src = ""; // Clear the iframe source
+}
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Escape") {
+    closePopup();
+  }
+});
+
+
